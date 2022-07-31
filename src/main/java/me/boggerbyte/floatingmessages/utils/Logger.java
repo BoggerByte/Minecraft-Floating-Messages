7aa70911453b53dee011399734fad2fa029f5030
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-31 02:07:05 +0200

    Simplified Logger

--- src/main/java/me/boggerbyte/floatingmessages/utils/Logger.java
+++ src/main/java/me/boggerbyte/floatingmessages/utils/Logger.java
@@ -1,13 +1,12 @@
 package me.boggerbyte.floatingmessages.utils;
 
 import me.boggerbyte.floatingmessages.Main;
-import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
+
+import java.util.logging.Level;
 
 public class Logger {
-    public static void log(String message) {
-        var console = Bukkit.getConsoleSender();
-        var pluginName = Main.getInstance().getName();
-        console.sendMessage(Component.text( "[" + pluginName + "] " + message));
+    public static void log(Level level, String message) {
+        var logger = Main.getInstance().getLogger();
+        logger.log(level, message);
     }
 }
