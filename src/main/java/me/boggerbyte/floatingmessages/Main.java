9021a56871625274944a05f9e5e9c43c9a044186
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-31 02:06:54 +0200

    Removed config manager

--- src/main/java/me/boggerbyte/floatingmessages/Main.java
+++ src/main/java/me/boggerbyte/floatingmessages/Main.java
@@ -3,43 +3,42 @@ package me.boggerbyte.floatingmessages;
 import me.boggerbyte.floatingmessages.floating_message.FloatingMessageFormatter;
 import me.boggerbyte.floatingmessages.floating_message.FloatingMessageSpawner;
 import me.boggerbyte.floatingmessages.listeners.ChatListener;
-import me.boggerbyte.floatingmessages.managers.ConfigManager;
 import me.boggerbyte.floatingmessages.utils.Logger;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
+import java.util.logging.Level;
 
 public final class Main extends JavaPlugin {
-    private ConfigManager configManager;
     private FloatingMessageSpawner floatingMessageSpawner;
 
     @Override
     public void onLoad() {
-        configManager = new ConfigManager(
-                "config.yml",
-                Arrays.asList(
-                        "max-lines-width",
-                        "max-lines-amount",
-                        "min-duration",
-                        "max-duration",
-                        "read-speed"));
-        floatingMessageSpawner = new FloatingMessageSpawner(
-                new FloatingMessageFormatter(
-                        configManager.getConfig().getInt("max-lines-width"),
-                        configManager.getConfig().getInt("max-lines-amount")),
-                configManager.getConfig().getInt("min-duration"),
-                configManager.getConfig().getInt("max-duration"),
-                configManager.getConfig().getInt("read-speed"));
+        saveDefaultConfig();
+
+        var config = getConfig();
+        var configRequiredFields = new String[]{
+                "max-lines-width",
+                "max-lines-amount",
+                "min-duration",
+                "max-duration",
+                "read-speed"
+        };
+        Arrays.stream(configRequiredFields).filter(field -> !config.contains(field, true))
+                .forEach(field -> Logger.log(Level.WARNING, "Missing required config field <" + field + ">. Using default value"));
     }
 
     @Override
     public void onEnable() {
-        var missingConfigFields = configManager.getMissingRequiredFields();
-        if (!missingConfigFields.isEmpty()) {
-            missingConfigFields.forEach(field -> Logger.log("Missing required config field <" + field + ">"));
-            return;
-        }
+        var messageFormatter = new FloatingMessageFormatter(
+                getConfig().getInt("max-lines-width"),
+                getConfig().getInt("max-lines-amount"));
+        floatingMessageSpawner = new FloatingMessageSpawner(
+                messageFormatter,
+                getConfig().getInt("min-duration"),
+                getConfig().getInt("max-duration"),
+                getConfig().getInt("read-speed"));
 
         getServer().getPluginManager().registerEvents(new ChatListener(floatingMessageSpawner), this);
     }
