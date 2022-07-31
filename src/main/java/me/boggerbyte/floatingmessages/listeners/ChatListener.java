2ddeeeacabaf5f3d4ed6822de9149ab098c25233
Nick Troshnev <nik.troshnev@gmail.com>
2022-07-31 15:10:28 +0200

    Rename FloatingMessageSpawner to FloatingMessageFactory

--- src/main/java/me/boggerbyte/floatingmessages/listeners/ChatListener.java
+++ src/main/java/me/boggerbyte/floatingmessages/listeners/ChatListener.java
@@ -2,20 +2,18 @@ package me.boggerbyte.floatingmessages.listeners;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
 import me.boggerbyte.floatingmessages.Main;
-import me.boggerbyte.floatingmessages.floating_message.FloatingMessageSpawner;
+import me.boggerbyte.floatingmessages.floating_message.FloatingMessageFactory;
 import net.kyori.adventure.text.TextComponent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 
 public class ChatListener implements Listener {
-
     private final Plugin plugin = Main.getInstance();
+    private final FloatingMessageFactory floatingMessageFactory;
 
-    private final FloatingMessageSpawner floatingMessageSpawner;
-
-    public ChatListener(FloatingMessageSpawner floatingMessageSpawner) {
-        this.floatingMessageSpawner = floatingMessageSpawner;
+    public ChatListener(FloatingMessageFactory floatingMessageFactory) {
+        this.floatingMessageFactory = floatingMessageFactory;
     }
 
     @EventHandler
@@ -25,6 +23,6 @@ public class ChatListener implements Listener {
 
         // running task in sync mode because event is asynchronous
         plugin.getServer().getScheduler()
-                .runTask(plugin, () -> floatingMessageSpawner.mountOn(player, chatMessage));
+                .runTask(plugin, () -> floatingMessageFactory.spawnOn(player, chatMessage));
     }
 }
